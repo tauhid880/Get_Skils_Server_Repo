b11c71f510c2e6511c9c1650474eb734e61e4b9f
tauhid880 <tauhid880@gmail.com>
2022-10-25 21:37:06 +0600

    some data change

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+app.use(cors());
 
+const Courses = require("./Data/Courses.json");
+
+// Single course data load here
+app.get("/courses/category", (req, res) => {
+  res.send(Courses);
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("Example app listening on port", port);
 });
