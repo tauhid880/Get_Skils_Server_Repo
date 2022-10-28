c76d7bee9ae9230f7f0ab40e78e8e9d5c74d06b6
tauhid880 <tauhid880@gmail.com>
2022-10-28 21:48:03 +0600

    final commit

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ app.use(cors());
 
 const Courses = require("./Data/Courses.json");
 
-// Single course data load here
+// Single course category data load here
 app.get("/courses/category", (req, res) => {
   res.send(Courses);
 });
+//  Single course data load here
+const course = require("./Data/courseData.json");
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selected_course = course.find((c) => c.id === id);
+  res.send(selected_course);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
